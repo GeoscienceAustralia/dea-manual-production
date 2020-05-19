0423db03d8239e7589c7566d82e3c92c9e8192dd
Duncan Gray <duncan.gray@ga.gov.au>
2020-05-20 08:05:58 +1000

    quick fix for code made for gap filling becoming production.

--- s2_l1/level1.sh
+++ s2_l1/level1.sh
@@ -12,23 +12,33 @@ END=$2
 INPUT_DIR=$3
 OUTPUT_DIR=$4
 
+# Fix me use the date type
 if [ "$END" = "2019-12-32" ]; then
     END=2020-01-01
-fi
-
-if [ "$END" = "2020-01-32" ]; then
+elif [ "$END" = "2020-01-32" ]; then
     END=2020-02-01
-fi
-
-if [ "$END" = "2020-02-30" ]; then
+elif [ "$END" = "2020-02-30" ]; then
     END=2020-03-01
-fi
-
-if [ "$END" = "2020-03-32" ]; then
+elif [ "$END" = "2020-03-32" ]; then
     END=2020-04-01
+elif [ "$END" = "2020-04-31" ]; then
+    END=2020-05-01
+elif [ "$END" = "2020-05-32" ]; then
+    END=2020-06-01
+elif [ "$END" = "2020-06-31" ]; then
+    END=2020-07-01
+elif [ "$END" = "2020-07-32" ]; then
+    END=2020-08-01
+elif [ "$END" = "2020-08-32" ]; then
+    END=2020-09-01
+elif [ "$END" = "2020-09-31" ]; then
+    END=2020-10-01
+elif [ "$END" = "2020-10-32" ]; then
+    END=2020-11-01
+elif [ "$END" = "2020-11-31" ]; then
+    END=2020-12-01
 fi
 
 echo $START $END
 echo $INPUT_DIR $OUTPUT_DIR
 ./s2-nci-processing_l1yaml.py generate-level1 --level1-root "$INPUT_DIR" --copy-parent-dir-count 1 --output-dir  "$OUTPUT_DIR" --start-date "$START"  --end-date "$END" --log-level "INFO"
-#s2-nci-processing generate-level1 --level1-root "$INPUT_DIR" --copy-parent-dir-count 1 --output-dir  "$OUTPUT_DIR" --start-date "$START"  --end-date "$END"
