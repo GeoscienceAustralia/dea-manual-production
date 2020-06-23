9fdc8c4e7eca8cff66e90c7d42bf76f1b8a07795
Duncan Gray <duncan.gray@ga.gov.au>
2020-06-23 12:09:43 +1000

    new production script.  Dropped some parameters that have defaults.

---
+++ landsat_c3/prod_ard_23062020.sh
@@ -0,0 +1,2 @@
+source /g/data/v10/Landsat-Collection-3-ops/OFFICIAL/Collection-3_5.4.1.env
+ard_pbs --level1-list /g/data/u46/users/dsg547/results/scenes_to_ARD_process_23062020.txt --workdir  /g/data/v10/projects/landsat_c3/wagl_workdir --logdir /g/data/v10/projects/landsat_c3/wagl_logdir --pkgdir /g/data/xu18/ga --env /g/data/v10/Landsat-Collection-3-ops/OFFICIAL/Collection-3_5.4.1.env --nodes 1 --project v10 --walltime 5:00:00
