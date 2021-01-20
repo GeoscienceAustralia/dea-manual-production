264441847c8172db92075958b3524ad46b231277
Duncan Gray <duncan.gray@ga.gov.au>
2021-01-20 14:16:43 +1100

    ready for production

--- landsat_c3/NOA-171/go_archiving.sh
+++ landsat_c3/NOA-171/go_archiving.sh
@@ -6,5 +6,5 @@ module use /g/data/v10/private/modules/modulefiles
 module load dea
 
 # indexing from ard
-cat bad_l1_uuids_to_archive.txt | parallel -j 8  -m -n 2 --line-buffer datacube  --config dsg547_dev.conf dataset archive 
+cat bad_l1_uuids_to_archive.txt | parallel -j 8  -m -n 2 --line-buffer datacube   dataset archive 
 
