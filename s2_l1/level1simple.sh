e938656eacd7bf0b9da3fade1cbf22c4a78765d6
Duncan Gray <duncan.gray@ga.gov.au>
2021-02-09 11:33:50 +1100

    Fixing how the dates are done

---
+++ s2_l1/level1simple.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+WAGL_VERSION='5.3.1-gadi'
+
+module use /g/data/v10/public/modules/modulefiles
+module use /g/data/v10/private/modules/modulefiles
+
+module load "wagl/${WAGL_VERSION}"
+
+START=$1
+END=$2
+INPUT_DIR=$3
+OUTPUT_DIR=$4
+
+echo $START $END
+echo $INPUT_DIR $OUTPUT_DIR
+./s2-nci-processing_l1yaml.py generate-level1 --level1-root "$INPUT_DIR" --copy-parent-dir-count 1 --output-dir  "$OUTPUT_DIR" --start-date "$START"  --end-date "$END" --log-level "INFO"
