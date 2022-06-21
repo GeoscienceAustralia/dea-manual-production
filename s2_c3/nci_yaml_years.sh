732fef0e31555f78029b43a541555cb5b9ce6974
Duncan Gray <duncan.gray@ga.gov.au>
2022-06-21 10:49:54 +1000

    Have a dry run flag in the production script.

--- s2_c3/nci_yaml_years.sh
+++ s2_c3/nci_yaml_years.sh
@@ -12,12 +12,6 @@ walltime="08:00:00"
 
 module="eodatasets3/0.28.3"
 inputdir="/g/data/fj7/Copernicus/Sentinel-2/MSI/L1C/"
-dry_run=" "
-index="--index "
-
-#Safety. Remove to generate into production.
-index=" "
-dry_run="--dry-run "
 
 project="v10"
 base_dir="/g/data/v10/work/s2_c3_ard"
@@ -30,8 +24,23 @@ config_arg="--config $SCRIPT_DIR/datacube.conf "
 
 verbose=" "
 
-year=2021
-months=(01 02 03 04 05 06 07 08 09 10 11 12)
+year=2022
+#months=(01 02 03 04 05 06 07 08 09 10 11 12)
+months=(01)
+
+# run ['dry'|'actual']
+run='dry'
+#run='actual'
+if [ "$run" = "actual" ]; then
+   dry_run=" "
+   index="--index "
+else
+   dry_run="--dry-run "
+   index=" "
+   ncpus="1 "
+   mem="20GB"
+   walltime="00:10:00"
+fi
 
 # Having the info above as variables and some empty values
 # means I can easily test by adding some test code here
@@ -45,27 +54,6 @@ months=(01 02 03 04 05 06 07 08 09 10 11 12)
 #inputdir="/g/data/fj7/Copernicus/Sentinel-2/MSI/L1C/2021/"
 config_arg="--config /g/data/u46/users/dsg547/sandbox/processingDEA/s2_pipeline/pipeline_test.conf"
 #config_arg="--config /g/data/u46/users/dsg547/sandbox/processingDEA/s2_pipeline/dsg547_dev.conf"
-
-# run ['dry'|'actual']
-run='dry'
-run='actual'
-if [ "$run" = "actual" ]; then
-   dry_run=" "
-   index="--index "
-   months=(07)
-   walltime="08:00:00"
-else
-   dry_run="--dry-run "
-   index=" "
-   ncpus="1 "
-   mem="20GB"
-   months=(01)
-   months=(02 03 04 05 06 07 08 09 10 11 12)
-   walltime="00:10:00"
-fi
-
-#SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" &>/dev/null && pwd)"
-#aoi=$SCRIPT_DIR/"53KQB.txt"
 
 # dsg547
 project="u46"
