ef06a749bd112fb183562afb08db76188891b15e
Duncan Gray <duncan.gray@ga.gov.au>
2022-07-11 17:52:46 +1000

    fast check

--- s2_c3/check.sh
+++ s2_c3/check.sh
@@ -10,13 +10,17 @@ ODCCONF=""
 
   
 echo product='esa_s2am_level1_0'
- datacube  $ODCCONF dataset search product='esa_s2am_level1_0' | grep '^id: ' | wc -l
+#time datacube  $ODCCONF dataset search product='esa_s2am_level1_0' | grep '^id: ' | wc -l
+python -c 'import datacube; dc=datacube.Datacube(); print(dc.index.datasets.count(product="esa_s2am_level1_0"))';
 
 echo product='esa_s2bm_level1_0'
-datacube  $ODCCONF dataset search product='esa_s2bm_level1_0' | grep '^id: ' | wc -l
+#datacube  $ODCCONF dataset search product='esa_s2bm_level1_0' | grep '^id: ' | wc -l
+python -c 'import datacube; dc=datacube.Datacube(); print(dc.index.datasets.count(product="esa_s2bm_level1_0"))';
 
 echo product='ga_s2am_ard_3'
-datacube  $ODCCONF dataset search product='ga_s2am_ard_3' | grep '^id: ' | wc -l
+#datacube  $ODCCONF dataset search product='ga_s2am_ard_3' | grep '^id: ' | wc -l
+python -c 'import datacube; dc=datacube.Datacube(); print(dc.index.datasets.count(product="ga_s2am_ard_3"))';
 
 echo product='ga_s2bm_ard_3'
-datacube  $ODCCONF dataset search product='ga_s2bm_ard_3' | grep '^id: ' | wc -l
+#datacube  $ODCCONF dataset search product='ga_s2bm_ard_3' | grep '^id: ' | wc -l
+python -c 'import datacube; dc=datacube.Datacube(); print(dc.index.datasets.count(product="ga_s2bm_ard_3"))';
