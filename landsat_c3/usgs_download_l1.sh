f3b392636a2f399eb10aa3561956a9d6f853c6e9
Duncan Gray <duncan.gray@ga.gov.au>
2023-11-13 14:33:00 +1100

    Add line for 2013 to 2021 downloading. Add comments for understanding. Removed commented lines.

--- landsat_c3/usgs_download_l1.sh
+++ landsat_c3/usgs_download_l1.sh
@@ -13,14 +13,16 @@ STER_LOG_DIR=${LOG_DIR}/${CURRENT_DATE}
 mkdir -p $STER_LOG_DIR;
 
 #PROD
-# Check with Duncan before downloading pre 2022 data
-export ENVIRONMENT=NCI-DEF-PROD;/home/547/lpgs/sandbox/usgsdownloader/usgsdownloader/go_usgs_downloader.sh --pipeline L8L1C2 --logPath $LOG_DIR --startDate 20231001 --endDate 20231016 &> ${STER_LOG_DIR}/extra_extent.log
-# /scratch/v10/USGSM2M/
-#  --oneScene
-# --dryrun
+# Used for downloading extra ls8/9 l1 c1 2022 - 2023
+# Only the extras region list.
+# Using the usgsdownloader repo branch DSNS-276
+#export ENVIRONMENT=NCI-DEF-PROD;/home/547/lpgs/sandbox/usgsdownloader/usgsdownloader/go_usgs_downloader.sh --pipeline L8L1C2 --logPath $LOG_DIR --startDate 20231001 --endDate 20231016 &> ${STER_LOG_DIR}/extra_extent.log
 
-# NCI-DEF-DEV
-# export ENVIRONMENT="NCI-DEF-DEV";$DIR/../usgsdownloader/go_usgs_downloader.sh --pipeline L7L1C2 --log INFO --logPath $SCRATCH --dryrun   --oneScene
-# export ENVIRONMENT="NCI-DEF-DEV";$DIR/../usgsdownloader/go_usgs_downloader.sh --pipeline L8L1C2 --log INFO --logPath $SCRATCH --dryrun   --startDate 20220101 --endDate 20220323 #  --oneScene
-# export ENVIRONMENT="LOCAL-DEV";$DIR/../usgsdownloader/go_usgs_downloader.sh --pipeline L8L1C2 --log INFO --logPath $SCRATCH --dryrun  --startDate 20220101 --endDate 20220317
-# /g/data/u46/users/dsg547/dump/LSL1C2
+#PROD
+# Used for downloading extra ls8/9 l1 c1 2013 - 2021
+# Reduced extras region list since ARDs have been produced, from l1 c1, for some of the extra regions. 
+# Using the usgsdownloader repo branch ls_ARD_filtered
+export ENVIRONMENT=NCI-DEF-PROD;/home/547/lpgs/sandbox/ls_ARD_filtered/usgsdownloader/usgsdownloader/go_usgs_downloader.sh --pipeline L8L1C2 --logPath $LOG_DIR --startDate 20211201 --endDate 202211202  &> ${STER_LOG_DIR}/extra_extent.log
+
+
+#  --dryrun --oneScene
