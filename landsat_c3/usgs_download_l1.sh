b00cf86fbfc089099e736e51833b152a4c09b7b2
Duncan Gray <duncan.gray@ga.gov.au>
2023-11-13 14:47:07 +1100

    add dryrun

--- landsat_c3/usgs_download_l1.sh
+++ landsat_c3/usgs_download_l1.sh
@@ -22,7 +22,7 @@ mkdir -p $STER_LOG_DIR;
 # Used for downloading extra ls8/9 l1 c1 2013 - 2021
 # Reduced extras region list since ARDs have been produced, from l1 c1, for some of the extra regions.
 # Using the usgsdownloader repo branch ls_ARD_filtered
-export ENVIRONMENT=NCI-DEF-PROD;/home/547/lpgs/sandbox/ls_ARD_filtered/usgsdownloader/usgsdownloader/go_usgs_downloader.sh --pipeline L8L1C2 --logPath $LOG_DIR --startDate 20211201 --endDate 202211202  &> ${STER_LOG_DIR}/extra_extent.log
+export ENVIRONMENT=NCI-DEF-PROD;/home/547/lpgs/sandbox/ls_ARD_filtered/usgsdownloader/usgsdownloader/go_usgs_downloader.sh --pipeline L8L1C2 --logPath $LOG_DIR --startDate 20211201 --endDate 202211202 --dryrun &> ${STER_LOG_DIR}/pre_2022_extra_extent.log
 
 
 #  --dryrun --oneScene
