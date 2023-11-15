1bd45ebafd110589abcff7a26ed23a7fbf8380cb
Duncan Gray <duncan.gray@ga.gov.au>
2023-11-15 16:03:23 +1100

    switch back to 2022 onward region

--- landsat_c3/usgs_download_l1.sh
+++ landsat_c3/usgs_download_l1.sh
@@ -16,13 +16,13 @@ mkdir -p $STER_LOG_DIR;
 # Used for downloading extra ls8/9 l1 c1 2022 - 2023
 # Only the extras region list.
 # Using the usgsdownloader repo branch DSNS-276
-#export ENVIRONMENT=NCI-DEF-PROD;/home/547/lpgs/sandbox/usgsdownloader/usgsdownloader/go_usgs_downloader.sh --pipeline L8L1C2 --logPath $LOG_DIR --startDate 20231001 --endDate 20231016 &> ${STER_LOG_DIR}/extra_extent.log
+export ENVIRONMENT=NCI-DEF-PROD;/home/547/lpgs/sandbox/usgsdownloader/usgsdownloader/go_usgs_downloader.sh --pipeline L8L1C2 --logPath $LOG_DIR --startDate 20231001 --endDate 20231016 &> ${STER_LOG_DIR}/extra_extent.log
 
 #PROD
 # Used for downloading extra ls8/9 l1 c1 2013 - 2021
 # Reduced extras region list since ARDs have been produced, from l1 c1, for some of the extra regions.
 # Using the usgsdownloader repo branch ls_ARD_filtered
-export ENVIRONMENT=NCI-DEF-PROD;/home/547/lpgs/sandbox/ls_ARD_filtered/usgsdownloader/usgsdownloader/go_usgs_downloader.sh --pipeline L8L1C2 --logPath $LOG_DIR --startDate 20211201 --endDate 20211202 &> ${STER_LOG_DIR}/pre_2022_extra_extent.log
+# export ENVIRONMENT=NCI-DEF-PROD;/home/547/lpgs/sandbox/ls_ARD_filtered/usgsdownloader/usgsdownloader/go_usgs_downloader.sh --pipeline L8L1C2 --logPath $LOG_DIR --startDate 20211201 --endDate 20211202 &> ${STER_LOG_DIR}/pre_2022_extra_extent.log
 
 
 #  --dryrun --oneScene
