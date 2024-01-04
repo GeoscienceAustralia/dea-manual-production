a7878c44302d2cec50340779d5ee59f7854b2b3e
Duncan Gray <duncan.gray@ga.gov.au>
2024-01-04 11:58:19 +1100

    fixing comments

--- landsat_c3/ls7_usgs_download_l1.sh
+++ landsat_c3/ls7_usgs_download_l1.sh
@@ -13,9 +13,9 @@ STER_LOG_DIR=${LOG_DIR}/${CURRENT_DATE}
 mkdir -p $STER_LOG_DIR;
 
 #PROD
-# Used for downloading extra ls8/9 l1 c1 1999 - 5 April 2022
+# Used for downloading extra ls7 l1 c2 1999 - 5 April 2022
 # The region  113_084 has been removed, since
-# it has been downloaded and ARD processed from 1999 to Dec 2021, for ls7.
+# it has to be downloaded from 1999 to Dec 2021, for ls7.
 # Using the usgsdownloader repo branch ls_ARD_filtered
 export ENVIRONMENT=NCI-DEF-PROD;/home/547/lpgs/sandbox/ls_ARD_filtered/usgsdownloader/usgsdownloader/go_usgs_downloader.sh --pipeline L7L1C2 --dryrun --logPath $LOG_DIR --startDate 20220301 --endDate 20220405 &> ${STER_LOG_DIR}/ls7_2022_extra_extent.log
 
